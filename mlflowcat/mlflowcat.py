d4447550e1a96aed04aab1f03a5d1990e6b26dc5
Alex Gregory <alex.gregory1@outlook.com>
2022-09-18 18:35:28 +0100

    Add function to test if run id is valid

--- mlflowcat/mlflowcat.py
+++ mlflowcat/mlflowcat.py
@@ -1,2 +1,7 @@
-print("Hello World")
+import re
 
+
+def is_valid_run_id(run_id: str):
+    """Return True if `run_id` is a valid mlflow run_id."""
+    pattern = r"^[0-9a-z]{32}$"
+    return bool(re.match(pattern, run_id))
