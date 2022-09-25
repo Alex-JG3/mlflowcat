5b7c50cb7e4f1ef58e507f58ee98ef2723d97ab5
Alex Gregory <alex.gregory1@outlook.com>
2022-09-25 16:31:46 +0100

    Initialise test for printing tables

--- tests/test_mlflowcat.py
+++ tests/test_mlflowcat.py
@@ -1,3 +1,5 @@
+import random
+import string
 import pytest
 from mlflowcat import mlflowcat
 
@@ -30,3 +32,49 @@ from mlflowcat import mlflowcat
 def test_is_valid_run_id(run_id):
     run_id = "73bf7b7b884a4f35be4d3c0b045a6898"
     assert mlflowcat.is_valid_run_id(run_id)
+
+
+@pytest.fixture
+def test_create_run(tmp_path):
+    """Create a toy mlflow run"""
+    run_id = "".join(
+        [random.choice(string.ascii_letters + string.digits) for _ in range(32)]
+    ).lower()
+
+    run_path = tmp_path / run_id
+    params_path = run_path / "params"
+    metrics_path = run_path / "metrics"
+
+    run_path.mkdir()
+    params_path.mkdir()
+    metrics_path.mkdir()
+
+    for i in range(10):
+        param_path = params_path / f"param_{i}"
+        with param_path.open("w", encoding="utf-8") as f:
+            f.write(f"value_{i}")
+
+    return run_path
+
+
+def test_mlflow_cat(test_create_run):
+    run_path = test_create_run
+    mlflowcat.mlflowcat(run_path)
+
+
+@pytest.mark.parametrize(
+    "dictionary",
+    [
+        {
+            "key_1": "value_1",
+            "key_2": "value_2",
+            "key_3": "value_3",
+            "key_4": "value_4",
+        }
+    ],
+)
+def test_table(dictionary):
+    table = mlflowcat.Table()
+    table.write_dictionary_to_table(dictionary)
+    print("\n")
+    print(table.table.getvalue())
